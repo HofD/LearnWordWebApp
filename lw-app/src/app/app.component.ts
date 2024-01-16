7543a148a3c228e56bb421c4c1ddd9b785cdae69
Alexey Karpov <Mr.HofD@gmail.com>
2024-01-16 07:45:11 +0300

    Added environment support. Added home and header components.

--- lw-app/src/app/app.component.ts
+++ lw-app/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

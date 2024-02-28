18e027acec678d05b1c6895581c42778ca9d40f1
Alexey Karpov <Mr.HofD@gmail.com>
2024-02-28 06:33:43 +0300

    Added delete card button.

--- lw-app/src/app/cards/cards.component.ts
+++ lw-app/src/app/cards/cards.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Card } from '../card/card';
 import { CardComponent } from '../card/card.component';
 import { NgFor } from '@angular/common';
+import { CardHttpService } from '../card/card.http.service';
 
 @Component({
   selector: 'app-cards',
@@ -14,7 +15,17 @@ export class CardsComponent {
   @Input() cards: Array<Card> = new Array<Card>;
   @Input() collectionId!: number;
 
+  constructor(
+    private http: CardHttpService
+  ) { }
+
   addCard(card: Card) {
     this.cards.push(card);
+  }
+
+  delete(id: number, index: number) {
+    this.http.delete(id).subscribe({
+      next: () => this.cards.splice(index, 1)
+    })
   }
 }
